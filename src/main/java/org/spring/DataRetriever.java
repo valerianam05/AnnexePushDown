7ef2a04cda550be2590fd61450568f6e5b532e92
valeria <hei.valeria.2@gmail.com>
2026-02-24 16:56:43 +0300

    feat: implement weighted turnover and tax summary calculations

--- src/main/java/org/spring/DataRetriever.java
+++ src/main/java/org/spring/DataRetriever.java
@@ -11,6 +11,101 @@ import java.util.List;
     // throw et throws
 
 public class DataRetriever {
+    public BigDecimal computeWeightedTurnoverTtc() {
+        BigDecimal result = BigDecimal.ZERO;
+
+        String sql = """
+        SELECT 
+            SUM(
+                (il.quantity * il.unit_price) * (1 + t.rate / 100.0) * CASE 
+                    WHEN i.status = 'PAID' THEN 1.0
+                    WHEN i.status = 'CONFIRMED' THEN 0.5
+                    ELSE 0 
+                END
+            ) as total_ttc_weighted
+        FROM invoice i
+        JOIN invoice_line il ON i.id = il.invoice_id
+        CROSS JOIN tax_config t
+        WHERE t.label = 'TVA STANDARD'
+    """;
+// On peut utilsier le methode qui calcule le HT et ce création de view en BD(correction)
+        try (Connection conn = new DBConnection().getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            if (rs.next()) {
+                result = rs.getBigDecimal("total_ttc_weighted");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public List<InvoiceTaxSummary> findInvoiceTaxSummaries() {
+        List<InvoiceTaxSummary> summaries = new ArrayList<>();
+
+        String sql = """
+        SELECT 
+            i.customer_name,
+            SUM(il.quantity * il.unit_price) as total_ht,
+            SUM(il.quantity * il.unit_price) * (t.rate / 100.0) as total_tva,
+            SUM(il.quantity * il.unit_price) * (1 + t.rate / 100.0) as total_ttc
+        FROM invoice i
+            JOIN invoice_line il ON i.id = il.invoice_id
+            CROSS JOIN tax_config t 
+        GROUP BY i.id, i.customer_name, t.rate
+    """;
+    // il y a ce BY ID on peut recuperer , tax_config (c'est à dire à traver
+        try (Connection conn = new DBConnection().getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                summaries.add(new InvoiceTaxSummary(
+                        rs.getString("customer_name"),
+                        rs.getDouble("total_ht"),
+                        rs.getDouble("total_tva"),
+                        rs.getDouble("total_ttc")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return summaries;
+    }
+
+    public Double computeWeightedTurnover() {
+        Double turnover = 0.0;
+        // Push-down : le calcul (1.0 * PAID + 0.5 * CONFIRMED) est fait par la base de données
+        String sql = """
+        SELECT 
+            SUM(CASE 
+                WHEN i.status = 'PAID' THEN (il.quantity * il.unit_price) * 1.0
+                WHEN i.status = 'CONFIRMED' THEN (il.quantity * il.unit_price) * 0.5
+                ELSE 0 
+            END) as weighted_total
+        FROM invoice i 
+        JOIN invoice_line il ON i.id = il.invoice_id; 
+   
+    """;
+        //pourqoi on doit faire *0 car loresque c'est draft pas comme ça donc  else*0.5
+         // et si non par defaut c'est 0 sur le FRADT pn se pas comment o0 else 0 end
+         //JOIN invoice_line il ON i.id = il.invoice_id 1<->1
+        //il y a ce methode
+
+        try (Connection conn = new DBConnection().getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            if (rs.next()) {
+                turnover = rs.getDouble("weighted_total");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return turnover;
+    }
 
     public InvoiceStatusTotals computeStatusTotals() {
         String sql = """
--- src/main/java/org/spring/Main.java
+++ src/main/java/org/spring/Main.java
@@ -1,17 +1,64 @@
 package org.spring;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.math.BigDecimal;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        DataRetriever retriever = new DataRetriever();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.println("\nQ5-B) Chiffre d’affaires TTC pondéré");
+        BigDecimal turnoverTtc = retriever.computeWeightedTurnoverTtc();
+        System.out.printf("%.2f%n", turnoverTtc);
+
+//        System.out.println("\n--- RÉSULTAT QUESTION 05 (TVA & TTC) ---");
+//        List<InvoiceTaxSummary> summaries = retriever.findInvoiceTaxSummaries();
+//
+//        for (int i = 0; i < summaries.size(); i++) {
+//            InvoiceTaxSummary s = summaries.get(i);
+//            // Affichage : 1 | Nom | HT 0.00 | TVA 0.00 | TTC 0.00
+//            System.out.printf("%d | %s | HT %.2f | TVA %.2f | TTC %.2f%n",
+//                    (i + 1),
+//                    s.customerName(),
+//                    s.totalHT(),
+//                    s.totalTVA(),
+//                    s.totalTTC());
+//        }
+
+            // Affichage Q3
+//            System.out.println("Q3 - Totaux cumulés par statut");
+//            InvoiceStatusTotals statusTotals = retriever.computeStatusTotals();
+//            System.out.println(statusTotals); // Utilise le toString() du record
+//
+//            // Affichage Q4
+//            System.out.println("\nQ4 - Chiffre d’affaires pondéré");
+//            Double weightedTurnover = retriever.computeWeightedTurnover();
+//            System.out.printf("%.2f%n", weightedTurnover);
         }
-    }
+
+
+//        System.out.println("findInvoiceTotals() :");
+//        List<InvoiceTotal> q1 = retriever.getInvoiceTotals();
+//        for (int i = 0; i < q1.size(); i++) {
+//            InvoiceTotal t = q1.get(i);
+//            // Format : 1 | Alice | 250.00
+//            System.out.printf("%d | %s | %.2f%n", (i + 1), t.getCustomerName(), t.getTotalAmount());
+//        }
+//
+//        System.out.println("\nQ2 - Total des factures confirmées et payées");
+//        System.out.println("Résultat attendu lors de l'affichage dans le Main :");
+//
+//        // --- AFFICHAGE QUESTION 02 ---
+//        List<InvoiceTotal> q2 = retriever.getConfirmedInvoiceTotals();
+//        for (int i = 0; i < q2.size(); i++) {
+//            InvoiceTotal t = q2.get(i);
+//            // Format avec Status : 1 | Alice | CONFIRMED | 250.00
+//            System.out.printf("%d | %s | %s | %.2f%n",
+//                    (i + 1),
+//                    t.getCustomerName(),
+//                    t.getStatus(),
+//                    t.getTotalAmount());
+//        }
+
 }
